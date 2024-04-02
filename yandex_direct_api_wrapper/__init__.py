4e955a3936e946ea6fa6d1f495ced226b06c9ca4
Oleg Ionov <ionov.dpmn@gmail.com>
2024-04-02 14:27:43 +0300

    Initial Commit

---
+++ yandex_direct_api_wrapper/__init__.py
@@ -0,0 +1,34 @@
+from yandex_direct_api.client import YandexDirect
+from yandex_direct_api.constants import (
+    ReportType,
+    DateRangeType,
+    ProcessingMode,
+    CampaignType,
+    CampaignState,
+    CampaignStatusSelection,
+    CampaignStatusPayment,
+    AdType,
+    AdState,
+    AdStatusSelection,
+    SearchStrategyPlacementTypes,
+    YesNo,
+    ExtensionStatus,
+)
+
+
+__all__ = [
+    'YandexDirect',
+    'ReportType',
+    'DateRangeType',
+    'ProcessingMode',
+    'CampaignType',
+    'CampaignState',
+    'CampaignStatusSelection',
+    'CampaignStatusPayment',
+    'AdType',
+    'AdState',
+    'AdStatusSelection',
+    'SearchStrategyPlacementTypes',
+    'YesNo',
+    'ExtensionStatus',
+]
